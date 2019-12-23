3f0c8c8f6ebc236fbe356ed56d8932d1a0eaeac5
wanmaoor <wanmao1284879375@outlook.com>
2019-12-23 11:27:56 +0800

    move

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 yarn build &&
 git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
-mv dist/* ./ &&
+mv dist/css dist/img dist/js ./ &&
 rm -rf dist;
 git add . &&
 git commit -m "update" &&
